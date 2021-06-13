bf435734f46935687785630a1da3debd1e98047d
raked <raked@users.noreply.github.com>
2021-06-13 01:02:38 -0400

    I don't give up
    but I am going to try to do this in discordjs

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import discord
+import random
 from dotenv import load_dotenv
 import os
 
 client = discord.Client()
+prefix = '$'
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,14 +18,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith(f'{prefix}hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('$cornbarge'):
-        i = 10
+    if message.content.startswith(f'{prefix}cornbarge'):
+        i = 4
         while i > 0:
             await message.channel.send('Wake up ' + str(message.author))
             i = i - 1
-        
+
+    if message.content.startswith(f'{prefix}roulette'):
+       randomMember = random.choice(discord.member.id)
+       print(randomMember)
+       #await randomMember.move_to(None)
 
 client.run(TOKEN)
